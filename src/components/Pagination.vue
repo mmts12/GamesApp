7439063cacf1493bf358070a9a691bf922758c06
mmts12 <mmts12@gmail.com>
2023-01-16 23:03:10 +0200

    add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,21 @@
+<script>
+import { def } from '@vue/shared';
+
+
+export default {
+    data(){
+        return {
+            currentPage :1,
+            itemsPerPage:12,
+            numberOfPages:3
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script>
+import { computed } from '@vue/runtime-core';
 export default {
   props: ['games'],
-  created() {
-    // console.log(console.log(JSON.parse(JSON.stringify(this.games))))
-    console.log(this.games);
+    data() {
+    return {
+      currentPage: 1,
+      numberOfPages:this.games.length / 12,
+      filteredGames :[]
+  }
+  },
+
+  computed:{
+    filterGames (){
+      this.filteredGames = this.games.slice(this.currentPage-1,12);
+      console.log(this.filteredGames)
+    }
+    },
+
+    methods:{
+      nextPage(page){
+        this.currentPage= page;
+        console.log(this.currentPage);
+      }
+    },
+
+  mounted() {
+    console.log(this.games.length);
+    console.log(this.filterGames)
+
   },
 };
 </script>
@@ -13,11 +37,16 @@ export default {
     <h2>System requirement App</h2>
     <main>
       <ul>
-      <li class="game-card" v-for="game in this.games" :key="game.id">
-         <span>{{ game.title }}</span> 
-         <img :src="game.thumbnail" alt="">
-      </li>
-    </ul>
+        <li class="game-card" v-for="game in this.filteredGames" :key="game.id">
+          <span>{{ game.title }}</span>
+          <img :src="game.thumbnail" alt="" />
+        </li>
+      </ul>
     </main>
+    <div class="paginate-btns">
+    <button @click="nextPage(i)"  v-for="(i, index) in this.numberOfPages" :key="index">
+     {{ i }}
+    </button>
+  </div>
   </div>
 </template>
